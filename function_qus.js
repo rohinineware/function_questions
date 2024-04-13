a944a408ccb465a944599068ceada54a1ebee09d
rohinineware <rohinineware86@gmail.com>
2024-04-13 12:48:53 +0530

    question solve by using function

---
+++ function_qus.js
@@ -0,0 +1,129 @@
+// Given length and breadth of a rectangle, return the area of the rectangle
+
+// function getRectangle(length,breath){
+//     return length * breath;
+// }
+
+// const num=getRectangle(10,20);
+// console.log("Area of the rectangle is :-" , num);
+
+
+
+
+
+// Take a number and print the square of the number
+
+// function getSquare(number){
+//     return number*number;
+// }
+
+// const num=getSquare (2);
+// console.log("Square of the number is :- ",num);
+
+
+
+
+// Take two numbers and only print the integer part when a is divided by b
+
+// function getInteger(a,b){
+//     return (Math.floor(a/b));
+// }
+
+// const int=getInteger(5,3);
+// console.log("here is the integer number:",int);
+
+
+
+
+// Take a number and print out the last digit of the number
+
+// function lastdigit(num){
+//     return num%10;
+// }
+
+// const num=lastdigit(34);
+// console.log("here is the last digit: ",num);
+
+
+
+
+// Take a number and find the cube of that number
+
+// function getCube(num){
+//     return num*num*num;
+// }
+
+// const num=getCube(2);
+// console.log("here is cube: ",num);
+
+
+
+
+
+// Take salary of five employees and print the average salary of all the employees
+
+// function getsalary(a,b,c,d,e){
+//     let salary=(a+b+c+d+e)/5;
+//     console.log("Avereage salary is: ",salary);
+// }
+// getsalary(1,2,3,4,5);
+
+
+
+
+// Take two numbers and swap them. Ex - a = 10, b=5; =&gt; a=5, b=10
+
+// function getswap(a, b) {
+//   a = a + b;
+//   b = a - b;
+//   a = a - b;
+//   console.log(a, b);
+// }
+
+// getswap(10, 5);
+
+
+
+// Take a number and print out the first digit of the number. Ex - 145 = 1, 789 = 7
+
+// function firstnumber(num){
+//     return (parseInt(num/10));
+// }
+// const result=firstnumber(789);
+// console.log(result);
+
+
+
+
+// Take a 3 digit number and print the reverse of that number. Ex - 235 = 532
+
+
+// function getReverse(num){
+//     let reverse=0;
+//     while(num>0){
+//         let remider=num%10;
+//         reverse=reverse*10+remider;
+//         num=Math.floor(num/10);
+//     }
+   
+//     return reverse;
+// }
+// console.log(getReverse(235));
+
+
+
+// Take a number and return the sum of the first and last digit. Ex - 10 = 1+0 = 1, 452 = 4+2=7
+
+// function getsum(num){
+//     let sum=0;
+//     let add=num%10;
+//     while (num>0){
+//         sum=sum+add;
+//         num=Math.floor(num/10);
+//     }
+//     return sum;
+// }
+// const net=getsum(452);
+// console.log(net);
+
+
